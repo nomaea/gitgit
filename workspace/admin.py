85e33c448c5030487d2c0de8f44bcb307489f549
nomaea <ryan141@naver.com>
2025-05-23 10:37:56 +0000

    그만

--- workspace/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import UploadedFile, FileLog
-
-admin.site.register(UploadedFile)
-admin.site.register(FileLog)
--- workspace/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.upload_and_list, name='upload_and_list'),    # 파일 업로드 및 목록 보기
-    path('delete/<str:file_id>/', views.delete_file, name='delete_file'),   # 파일 삭제 요청
-    path('download/<str:file_id>/', views.download_file, name='download_file'), # 파일 다운로드 요청
-]
